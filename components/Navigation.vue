61ffc62c3187f12c461892c0cd65f910505aa6c1
Andrey Porodnov <andreyrep17@gmail.com>
2019-06-29 10:10:13 +0300

    add create button

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -11,37 +11,61 @@
       </ul>
     </div>
     <div class="area">
+      <div class="info-text"
+       v-if = "(!show_createForm)"
+       >
+        <div class="addNote"
+         @click="onAdd">
+          <img src="~/static/plus.svg" alt="">
+        </div>
+        <p>
+          Нажмите чтобы добавить новую заметку
+        </p>
+      </div>
       <div>
+        <input type="text"
+         class="input-title"
+         v-if="show_createForm"
+         v-model="elem.name">
+        <input type="text"
+         class="input-title text-body"
+         v-if="show_createForm"
+         v-model="elem.descript">
         <input type="text"
          class="input-title"
          v-if="show"
          v-model="elem.name">
         <input type="text"
-         class="input-body"
+         class="input-title text-body"
          v-if="show"
          v-model ="elem.descript">
          <div v-else>
             <h2>{{elem.name}}</h2>
             <p>{{elem.descript}}</p>
-            
          </div>
       </div>
        <button
         type="button"
         class="btn btn-success"
-        v-if = "show"
-        @click ="onSave(elem.id, elem.name, elem.descript, notes.notes)">Success</button>
+        v-if = "show||show_createForm"
+        @click ="onSave(elem.id, elem.name, elem.descript, notes.notes)">Save</button>
         <button
         type="button"
         class="btn btn-danger"
-        v-if = "show"
-        @click = "show = false">Cancel</button>
+        v-if = "show||show_createForm"
+        @click = "(show = false)&(show_createForm = false)">Cancel</button>
       <button
        type = "button"
        class = "btn btn-warning"
        v-if = "(elem.name != null)"
        v-show = "!show"
        @click = "show = true">Edit</button>
+      <button
+       type = "button"
+       class = "btn btn-danger"
+       v-if = "(elem.name != null)"
+       v-show = "!show"
+       @click ="onDelete(elem.id)">Delete</button>
     </div>
   </div>
 </template>
@@ -58,6 +82,7 @@ export default {
           descript: null
         },
         show: false,
+        show_createForm: false
       }
     },
     computed: mapState([
@@ -69,14 +94,18 @@ export default {
         this.$store.dispatch('saveNote', {id: idNote, name: nameNote, descript: descriptNote});
         this.$store.dispatch('postNote', notes_obj)
       },
-      test(notes_obj){
+      onAdd(){
+        this.show_createForm = true
       },
       onClick(note) {
-        if (!this.show){
+        if ((!this.show)&&(!this.show_createForm)){
         this.elem.name = note.name;
         this.elem.descript = note.descript;
         this.elem.id = note.id
         }
+      },
+      onDelete (id) {
+        this.$store.dispatch('deleteNote', id)
       }
     }
 }
@@ -84,24 +113,42 @@ export default {
 
 <style scoped lang="sass">
 .navigation
-    background-color: yellow
-    width: 30%
-    margin: 0 10px
-    height: 550px;
+  background-color: yellow
+  width: 30%
+  margin: 0 10px
+  height: 550px;
 li
-    margin-top: 10px
+  margin-top: 10px
 .area
-    background-color: green
-    width: 70%
-    margin: 0 10px
-    padding: 20px
-    color: white
+  background-color: green
+  width: 70%
+  margin: 0 10px
+  padding: 20px
+  color: white
 .index
-    display: flex
-    justify-content: flex-start
-    min-height: 500px
+  display: flex
+  justify-content: flex-start
+  min-height: 500px
 .input-title
   margin-bottom: 10px
+.input-title
+  width: 80%
+  height: 50px
+  margin-bottom: 20px
+  font-size: 20px
 button
   margin: 10px
+.info-text
+  min-height: 100%
+  display: flex
+  flex-direction: column
+  justify-content: center
+  align-items: center
+.info-text p 
+  font-size: 40px
+  text-align: center
+  width: 400px
+.addNote
+  width: 100px
+  height: 100px
 </style>
--- store/index.js
+++ store/index.js
@@ -8,13 +8,26 @@ export const mutations = {
         state.notes = notes
     },
     SAVE_NOTE(state, editNote){
-        state.notes.notes = state.notes.notes.map(note => {
-            if (note.id === editNote.id) {
-              note.name = editNote.name;
-              note.descript = editNote.descript;
-            } return note;
-    })
-}}
+        if (editNote.id == null) {
+            editNote.id == state.notes.notes.length;
+            state.notes.notes.push(editNote);
+        } else {
+            state.notes.notes = state.notes.notes.map(note => {
+                if (note.id === editNote.id) {
+                  note.name = editNote.name;
+                  note.descript = editNote.descript;
+                } return note;
+        })
+        }
+    },
+//     DELETE_NOTE (state, id) {
+//         state.notes.notes = state.notes.notes.map(note => {
+//             if (note.id === id) {
+//               state.notes.notes.splice(state.notes.notes.indexOf(note),1);
+//             } return note;
+//     }) 
+// }
+}
 export const actions = {
     async nuxtServerInit({ commit }){
         const response = await axios.get("https://script.google.com/macros/s/AKfycbwyaj--oDaTwwWk35EyvFcrRedCS07E9YVwTUql/exec")
@@ -37,6 +50,9 @@ export const actions = {
     },
     saveNote({commit}, editNote) {
         commit('SAVE_NOTE', editNote)
-    }
+    },
+    // deleteNote({commit}, id) {
+    //     commit('DELETE_NOTE', id)
+    // }
     
 }
